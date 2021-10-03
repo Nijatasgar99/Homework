b0d82925b6362a8d17c2f8f95153f84490195c79
Nijatasgar99 <nijataas@code.edu.az>
2021-10-04 00:02:25 +0500

    Добавьте файлы проекта.

---
+++ Nijat/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Nijat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 
+
+            // tek eded say
+            //int num1 = 20;
+            //int num2 = 1;
+            //int count = 0;
+            //for (int i = num2; i <= num1; i++)
+            //{
+            //    if (i % 2 == 1) ;
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+            #endregion
+
+            #region
+            //sade murekkeb
+            //int n = 45;
+            //int s = 0;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if (n % i == 0)
+            //    {
+            //        s++;
+            //    }
+
+            //}
+            //if (s> 2)
+            //{
+            //    Console.WriteLine("murekkeb");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" sadedir");
+            //}
+            #endregion
+
+            #region
+            //tek eded say
+            //int n = 65;
+            //int m = 95;
+            //int Z = 0;
+            //for (int i = n; i <= m; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        Z++;
+            //    }
+
+            //}
+            //Console.WriteLine(Z);
+            #endregion
+
+            #region
+            //nece mertebe oldugu mueyen olunmalidir
+            //int n = 1603;
+            //int hes = 0;
+            //if (n != 0)
+            //{
+            //    while (n >= 1)
+            //    {
+            //        n /= 10;
+            //        hes++;
+            //    }
+            //    Console.WriteLine(hes);
+            //}
+            //Console.WriteLine("mertebe sayi");
+            #endregion
+
+           
+        }
+    }
+}
